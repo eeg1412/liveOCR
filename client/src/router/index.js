ef1162dab95522ddc535fab47cfee50eaaea73f5
eeg1412 <eeg1412@gmail.com>
2021-10-01 16:57:13 +0900

    init2

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -7,11 +7,6 @@ const routes = [
     name: 'home',
     component: Home
   },
-  {
-    path: '/chat',
-    name: 'chat',
-    component: () => import(/* webpackChunkName: "chat" */ '../views/Chat.vue')
-  }
 ]
 
 const router = createRouter({
